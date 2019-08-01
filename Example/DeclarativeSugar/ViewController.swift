18fa062cd0ff6900b38897ffbe146ae0bbec99ad
Darren Zheng <623767307@qq.com>
2019-08-01 16:22:11 +0800

    example

--- Example/DeclarativeSugar/ViewController.swift
+++ Example/DeclarativeSugar/ViewController.swift
@@ -11,12 +11,77 @@ import SnapKit
 import DeclarativeSugar
 import Then
 
+
+// Full Reload Example
+
+//class ViewController: DeclarativeViewController {
+//
+//    var hide = false
+//
+//    override func build() -> DZStack {
+//        return DZColumn(
+//            crossAxisAlignment: .leading,
+//            children: [
+//                DZRow(
+//                    crossAxisAlignment: .bottom,
+//                    children: [
+//                        UILabel().then { $0.text = "hello world"; $0.isHidden = self.hide },
+//                        UIView().then {
+//                            $0.backgroundColor = .red
+//                            $0.snp.makeConstraints { make in
+//                                make.size.equalTo(CGSize(width: 50, height: 100))
+//                            }
+//                        },
+//                        DZSpacer(40),
+//                        UIView().then {
+//                            $0.backgroundColor = .blue
+//                            $0.snp.makeConstraints { make in
+//                                make.size.equalTo(CGSize(width: 50, height: 50))
+//                            }
+//                        },
+//                    ]),
+//                DZSpacer(self.hide ? 10 : 50),
+//                UIView().then {
+//                    $0.backgroundColor = .orange
+//                    $0.snp.makeConstraints { make in
+//                        make.size.equalTo(CGSize(width: 50, height: 50))
+//                    }
+//                },
+//                DZSpacer(10),
+//                UILabel().then {
+//                    $0.numberOfLines = 0
+//                    $0.text =
+//                    """
+//                    Swift is a general-purpose, multi-paradigm, compiled programming language created for iOS, OS X, watchOS, tvOS and Linux development by Apple Inc. Swift is designed to work with Apple's Cocoa and Cocoa Touch frameworks and the large body of existing Objective-C code written for Apple products
+//                    """
+//                },
+//            ])
+//            .then {
+//                view.addSubview($0)
+//                $0.snp.makeConstraints {
+//                    $0.top.equalToSuperview().offset(300)
+//                    $0.left.right.equalToSuperview()
+//                }
+//        }
+//    }
+//
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        // full reload
+//        self.rebuild {
+//            self.hide = !self.hide
+//        }
+//    }
+//
+//}
+
+// Incremental Reload Example
+
 class ViewController: DeclarativeViewController {
     
     var label = UILabel().then { $0.text = "hello world" }
     var spacer = DZSpacer(10);
     var hide = false
-
+    
     override func build() -> DZStack {
         return DZColumn(
             crossAxisAlignment: .leading,
@@ -24,22 +89,22 @@ class ViewController: DeclarativeViewController {
                 DZRow(
                     crossAxisAlignment: .bottom,
                     children: [
-                        UILabel().then { $0.text = "hello world"; $0.isHidden = self.hide }, // label
+                        label,
                         UIView().then {
                             $0.backgroundColor = .red
                             $0.snp.makeConstraints { make in
                                 make.size.equalTo(CGSize(width: 50, height: 100))
                             }
                         },
-                        DZSpacer(40), //spacer
+                        DZSpacer(10),
                         UIView().then {
-                            $0.backgroundColor = .blue
-                            $0.snp.makeConstraints { make in
-                                make.size.equalTo(CGSize(width: 50, height: 50))
-                            }
+                        $0.backgroundColor = .blue
+                        $0.snp.makeConstraints { make in
+                        make.size.equalTo(CGSize(width: 50, height: 50))
+                        }
                         },
                     ]),
-                DZSpacer(self.hide ? 10 : 50),
+                spacer,
                 UIView().then {
                     $0.backgroundColor = .orange
                     $0.snp.makeConstraints { make in
@@ -70,17 +135,12 @@ class ViewController: DeclarativeViewController {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        // full reload
-         self.rebuild {
-              self.hide = !self.hide
-         }
-        
         // incremental reload
-//        UIView.animate(withDuration: 0.5) {
-//            self.hide = !self.hide
-//            self.context.setSpacing(self.hide ? 50 : 10, for: self.spacer)
-//            self.context.setHidden(self.hide, for: self.label)
-//        }
+        UIView.animate(withDuration: 0.5) {
+            self.hide = !self.hide
+            self.context.setSpacing(self.hide ? 50 : 10, for: self.spacer)
+            self.context.setHidden(self.hide, for: self.label)
+        }
     }
     
     
