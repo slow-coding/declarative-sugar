a7a6b3db0bef3bd55e6d00148d625309033e454e
Darren Zheng <623767307@qq.com>
2019-08-02 17:44:06 +0800

    0.1.4

--- DeclarativeSugar/Classes/DeclarativeUI.swift
+++ DeclarativeSugar/Classes/DeclarativeUI.swift
@@ -127,7 +127,7 @@ public class DZRow: UIView, DZStack {
     
     public var children: [DZStackItem]
     public var stackView = UIStackView()
-    public init(mainAxisAlignment: UIStackView.Distribution = .fill,
+    public init(mainAxisAlignment: UIStackView.Distribution = .fillProportionally,
                 crossAxisAlignment: UIStackView.Alignment = .top,
                 children: [DZStackItem]) {
         self.children = children
@@ -267,5 +267,138 @@ public extension UIStackView {
             }
         }
     }
+    
+}
+
+
+public class DZListSection: NSObject {
+    public var headerView: UIView? = nil
+    public var footerView: UIView? = nil
+    public var headerTitle: String? = nil
+    public var footerTitle: String? = nil
+    public var headerHeight: CGFloat = 0
+    public var footerHeight: CGFloat = 0
+    public var rows: [DZListCell] = []
+    
+    public init(rows: [DZListCell]) {
+        self.rows = rows
+    }
+}
+
+public class DZListCell: NSObject {
+    public var stack: DZStack
+    public var configureCell: ((UITableViewCell) -> Void)? = nil
+    public var identifier: String = String(Int.random(in: 0 ... 9999))
+    public var cellClass: AnyClass? = nil
+    public var height: CGFloat? = nil
+    public var estimatedHeight: CGFloat? = nil
+    public var onTap: ((IndexPath) -> Void)? = nil
+    public var willDisplay: ((IndexPath) -> Void)? = nil
+    public var shouldHighlightRow: ((IndexPath) -> Void)? = nil
+    private var currentCell: UITableViewCell? = nil
+    
+    public init(stack: DZStack) {
+        self.stack = stack
+    }
+    
+}
+
+
+public class DZListView: UIView {
+    
+    let sections: [DZListSection]
+    let tableView: UITableView
+    
+    public init(tableView: UITableView, sections: [DZListSection]) {
+        self.sections = sections
+        self.tableView = tableView
+        
+        super.init(frame: .zero)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableView.automaticDimension
+        addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[tableView]|", options: .directionMask, metrics: nil, views: ["tableView":tableView]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[tableView]|", options: .directionMask, metrics: nil, views: ["tableView":tableView]))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+extension DZListView: UITableViewDataSource, UITableViewDelegate {
+    
+    public func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return sections[section].headerView
+    }
+    
+    public func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return sections[section].footerView
+    }
+    
+    public func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section].headerTitle
+    }
+    
+    public func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return sections[section].footerTitle
+    }
+    
+    public func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return sections[section].footerHeight
+    }
+    
+    public func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return sections[section].headerHeight
+    }
+    
+    public func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sections[section].rows.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = sections[indexPath.section].rows[indexPath.row]
+        let identifier = row.identifier
+        var cell = tableView.dequeueReusableCell(withIdentifier: identifier)
+        if cell == nil {
+            cell = DZListBaseCell(row: row, style: UITableViewCell.CellStyle.default, reuseIdentifier: identifier)
+        }
+        row.configureCell?(cell!)
+        return cell!
+    }
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let row = sections[indexPath.section].rows[indexPath.row]
+        row.onTap?(indexPath)
+    }
+}
+
+public class DZListBaseCell: UITableViewCell {
+    
+    let row: DZListCell
+    init(row: DZListCell, style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        self.row = row
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        if let stack = DZContext(child: row.stack).child as? UIView {
+            contentView.addSubview(stack)
+            stack.translatesAutoresizingMaskIntoConstraints = false
+            contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[stack]|", options: .directionMask, metrics: nil, views: ["stack":stack]))
+            contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[stack]|", options: .directionMask, metrics: nil, views: ["stack":stack]))
+        }
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     
 }
--- Example/DeclarativeSugar/ViewController.swift
+++ Example/DeclarativeSugar/ViewController.swift
@@ -79,74 +79,133 @@ import Then
 // Incremental Reload Example
 /////////////////////////////////////////////////////
 
+//class ViewController: DeclarativeViewController {
+//
+//    var label = UILabel().then { $0.text = "hello world" }
+//    var spacer = DZSpacer(10);
+//    var hide = false
+//
+//    override func build() -> DZStack {
+//        return DZColumn(
+//            crossAxisAlignment: .leading,
+//            children: [
+//                DZRow(
+//                    crossAxisAlignment: .bottom,
+//                    children: [
+//                        label,
+//                        UIView().then {
+//                            $0.backgroundColor = .red
+//                            $0.snp.makeConstraints { make in
+//                                make.size.equalTo(CGSize(width: 50, height: 100))
+//                            }
+//                        },
+//                        DZSpacer(10),
+//                        UIView().then {
+//                            $0.backgroundColor = .blue
+//                            $0.snp.makeConstraints { make in
+//                                make.size.equalTo(CGSize(width: 50, height: 50))
+//                            }
+//                        },
+//                    ]),
+//                spacer,
+//                UIView().then {
+//                    $0.backgroundColor = .orange
+//                    $0.snp.makeConstraints { make in
+//                        make.size.equalTo(CGSize(width: 50, height: 50))
+//                    }
+//                },
+//                DZSpacer(10),
+//                UILabel().then {
+//                    $0.numberOfLines = 0
+//                    $0.text =
+//                    """
+//                    Swift is a general-purpose, multi-paradigm, compiled programming language created for iOS, OS X, watchOS, tvOS and Linux development by Apple Inc. Swift is designed to work with Apple's Cocoa and Cocoa Touch frameworks and the large body of existing Objective-C code written for Apple products
+//                    """
+//                },
+//            ])
+//            .then {
+//                view.addSubview($0)
+//                $0.snp.makeConstraints {
+//                    $0.top.equalToSuperview().offset(300)
+//                    $0.left.right.equalToSuperview()
+//                }
+//        }
+//    }
+//
+//
+//
+//
+//
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//
+//        // incremental reload
+//        UIView.animate(withDuration: 0.5) {
+//            self.hide = !self.hide
+//            self.context.setSpacing(self.hide ? 50 : 10, for: self.spacer)
+//            self.context.setHidden(self.hide, for: self.label)
+//        }
+//    }
+//
+//
+//}
+//
+//
+
+
+
 class ViewController: DeclarativeViewController {
     
-    var label = UILabel().then { $0.text = "hello world" }
-    var spacer = DZSpacer(10);
-    var hide = false
-    
-    override func build() -> DZStack {
-        return DZColumn(
-            crossAxisAlignment: .leading,
-            children: [
-                DZRow(
-                    crossAxisAlignment: .bottom,
-                    children: [
-                        label,
-                        UIView().then {
-                            $0.backgroundColor = .red
-                            $0.snp.makeConstraints { make in
-                                make.size.equalTo(CGSize(width: 50, height: 100))
-                            }
-                        },
-                        DZSpacer(10),
-                        UIView().then {
-                        $0.backgroundColor = .blue
-                        $0.snp.makeConstraints { make in
-                        make.size.equalTo(CGSize(width: 50, height: 50))
-                        }
-                        },
-                    ]),
-                spacer,
-                UIView().then {
-                    $0.backgroundColor = .orange
-                    $0.snp.makeConstraints { make in
-                        make.size.equalTo(CGSize(width: 50, height: 50))
-                    }
-                },
-                DZSpacer(10),
-                UILabel().then {
-                    $0.numberOfLines = 0
-                    $0.text =
-                    """
-                    Swift is a general-purpose, multi-paradigm, compiled programming language created for iOS, OS X, watchOS, tvOS and Linux development by Apple Inc. Swift is designed to work with Apple's Cocoa and Cocoa Touch frameworks and the large body of existing Objective-C code written for Apple products
-                    """
-                },
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        _ = DZListView(
+            tableView: UITableView().then { $0.separatorStyle = .none },
+            sections: [
+                DZListSection(
+                    rows: [
+                        DZListCell(stack: DZColumn(children: [
+                            UILabel().then { $0.text = "hello world" },
+                            DZSpacer(20),
+                            ])),
+                        DZListCell(stack: DZColumn(children: [
+                            UILabel().then {
+                                $0.numberOfLines = 0
+                                $0.text =
+                                """
+                                群机器人是钉钉群的高级扩展功能。群机器人可以将第三方服务的信息聚合到群聊中，实现自动化的信息同步。
+                                目前，大部分机器人在添加后，还需要进行Webhook配置，才可正常使用(配置说明详见操作流程中的帮助链接)。
+                                """
+                            },
+                            DZSpacer(20),
+                            UISwitch(),
+                            DZSpacer(20),
+                            ])),
+                    ]).then { $0.headerTitle = "section 1"; $0.headerHeight = 20 },
+                DZListSection(
+                    rows: [
+                        DZListCell(stack:
+                            DZRow(children: [
+                                DZSpacer(20),
+                                UIImageView(image: UIImage(named: "1")).then( {$0.snp.makeConstraints {
+                                    $0.size.equalTo(CGSize(width: 50, height: 50))
+                                    }}),
+                                DZSpacer(20),
+                                DZColumn(children: [
+                                    UILabel().then { $0.text = "name"},
+                                    DZSpacer(5),
+                                    UILabel().then { $0.text = "1868888888"; $0.textColor = .gray },
+                                    ])
+                                ])).then { $0.configureCell = { cell in cell.accessoryType = .disclosureIndicator }},
+                    ]).then { $0.headerTitle = "section 2"; $0.headerHeight = 20 }
             ])
             .then {
                 view.addSubview($0)
                 $0.snp.makeConstraints {
-                    $0.top.equalToSuperview().offset(300)
-                    $0.left.right.equalToSuperview()
+                    $0.edges.equalToSuperview()
+                    
                 }
         }
     }
     
-    
-    
-    
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        // incremental reload
-        UIView.animate(withDuration: 0.5) {
-            self.hide = !self.hide
-            self.context.setSpacing(self.hide ? 50 : 10, for: self.spacer)
-            self.context.setHidden(self.hide, for: self.label)
-        }
-    }
-    
-    
 }
-
 
